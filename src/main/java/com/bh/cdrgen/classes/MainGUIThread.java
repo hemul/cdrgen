acce377ec71b8b3dc98a6ecb3abf7f9f9f42c91c
hemul <hemul@pochtamt.ru>
2013-01-12 14:30:00 +0300

    Changes related to Initial Setup Window.

--- src/main/java/com/bh/cdrgen/classes/MainGUIThread.java
+++ src/main/java/com/bh/cdrgen/classes/MainGUIThread.java
@@ -1,8 +1,13 @@
 package com.bh.cdrgen.classes;
 
+import java.io.File;
+
 import com.bh.cdrgen.gui.MainAppWindow;
 
 public class MainGUIThread implements Runnable{
+	
+	private final String PATH_TO_GLOBAL_CONFIGURATION_FILE = "/home/hemul/Programming/workspace/cdrgen/etc/cdrgen.xml";
+	
 	public void run() {
 		try {
 			MainAppWindow mainWindow = new MainAppWindow();
@@ -14,7 +19,25 @@ public class MainGUIThread implements Runnable{
 			e.printStackTrace();
 		}
 	}
+	
 	private boolean doesGlobalConfigurationFileExist(){
-		return false;
+		
+		int indx = PATH_TO_GLOBAL_CONFIGURATION_FILE.lastIndexOf( File.separator );
+		
+		String path = PATH_TO_GLOBAL_CONFIGURATION_FILE.substring( 0, indx);
+		String fileName = PATH_TO_GLOBAL_CONFIGURATION_FILE.substring( indx+1 );
+		
+		File directory = new File( path );
+		
+		boolean found = false;
+		for( File f : directory.listFiles() )
+		{
+		  if( f.getName().equals( fileName ) )
+		  {
+		    found = true;
+		    break;
+		  }
+		}
+		return found;
 	}
 }
